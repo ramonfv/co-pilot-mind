bd779482c0a7d59f3693a50a63d1cbe8d9ed9da3
ramonfv <ramon.fv95@gmail.com>
2025-04-04 15:04:07 -0300

    feat: add official transcription from youtube video, based on automatic subtitle

--- youtubeDownloader/downloader.py
+++ youtubeDownloader/downloader.py
@@ -34,6 +34,55 @@ class YouTubeAudioDownloader:
         ]
         subprocess.run(cmd, check=True)
         return output_path
+    
+    def download_transcript(self, language="pt") -> tuple[Path | None, Path | None]:
+        filename = self.get_video_title()
+        filename = self.sanitize_filename(filename)
+        vtt_path = self.output_dir / f"{filename}.{language}.vtt"
+        txt_path = self.output_dir / f"{filename}_with_timestamps.txt"
+
+        if txt_path.exists() and vtt_path.exists():
+            print(f"✅ Transcrição já existe: {txt_path.name}")
+            return vtt_path, txt_path
+
+        cmd = [
+            "yt-dlp",
+            "--write-auto-sub",
+            "--sub-lang", language,
+            "--skip-download",
+            "-o", str(self.output_dir / f"{filename}.%(ext)s"),
+            self.url
+        ]
+        subprocess.run(cmd, check=True)
+
+        if not vtt_path.exists():
+            print("⚠️ Nenhuma legenda automática encontrada.")
+            return None, None
+
+        with open(vtt_path, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+
+        output_lines = []
+        timestamp = ""
+
+        for line in lines:
+            line = line.strip()
+            if "-->" in line:
+                timestamp = line.split(" --> ")[0]
+                timestamp = timestamp.replace(".", ",") 
+                continue
+            elif line and not line.startswith("WEBVTT") and not re.match(r"^\d+$", line):
+                output_lines.append(f"[{timestamp}] {line}")
+
+        with open(txt_path, 'w', encoding='utf-8') as f:
+            f.write("\n".join(output_lines))
+
+        print(f"📝 Arquivos de transcrição salvos:")
+        print(f"  • VTT original: {vtt_path}")
+        print(f"  • TXT com timestamps: {txt_path}")
+
+        return vtt_path, txt_path
+
 
     def convert_to_wav(self, input_path: Path, output_name=None) -> Path:
         output_name = self.get_video_title() + ".wav" if output_name is None else output_name
--- youtubeDownloader/main.py
+++ youtubeDownloader/main.py
@@ -2,8 +2,9 @@ from downloader import YouTubeAudioDownloader
 
 def run():
     yt = YouTubeAudioDownloader("https://www.youtube.com/watch?v=ABhe_bDbplg")
-    mp3_path = yt.download_audio()
-    wav_path = yt.convert_to_wav(mp3_path)
+    # mp3_path = yt.download_audio()
+    # wav_path = yt.convert_to_wav(mp3_path)
+    vtt, txt = yt.download_transcript()
 
 if __name__ == "__main__":
     run()
